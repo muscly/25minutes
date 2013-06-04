a1f2edbb50c89374bca7791324d8796a4dfcd52c
Hyun Chang Lee <musclyhc@gmail.com>
2013-06-04 16:12:27 +0900

    Initial implementaion

---
+++ RodomopoTimer/MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace RodomopoTimer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelLeftTime = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timerUpdateLeftTime = new System.Windows.Forms.Timer(this.components);
+            this.timerCheckMouseLeave = new System.Windows.Forms.Timer(this.components);
+            this.timerShake = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelLeftTime
+            // 
+            this.labelLeftTime.AutoSize = true;
+            this.labelLeftTime.Font = new System.Drawing.Font("Impact", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLeftTime.Location = new System.Drawing.Point(12, 9);
+            this.labelLeftTime.Name = "labelLeftTime";
+            this.labelLeftTime.Size = new System.Drawing.Size(112, 48);
+            this.labelLeftTime.TabIndex = 0;
+            this.labelLeftTime.Text = "00:00";
+            this.labelLeftTime.MouseEnter += new System.EventHandler(this.labelLeftTime_MouseEnter);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.Location = new System.Drawing.Point(131, 9);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(83, 48);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "&Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timerUpdateLeftTime
+            // 
+            this.timerUpdateLeftTime.Interval = 1000;
+            this.timerUpdateLeftTime.Tick += new System.EventHandler(this.timerUpdateLeftTime_Tick);
+            // 
+            // timerCheckMouseLeave
+            // 
+            this.timerCheckMouseLeave.Interval = 300;
+            this.timerCheckMouseLeave.Tick += new System.EventHandler(this.timerCheckMouseLeave_Tick);
+            // 
+            // timerShake
+            // 
+            this.timerShake.Interval = 50;
+            this.timerShake.Tick += new System.EventHandler(this.timerShake_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 65);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelLeftTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.Text = "Rodomopo Timer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLeftTime;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Timer timerUpdateLeftTime;
+        private System.Windows.Forms.Timer timerCheckMouseLeave;
+        private System.Windows.Forms.Timer timerShake;
+    }
+}
+
---
+++ RodomopoTimer/MainForm.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RodomopoTimer
+{
+    public partial class MainForm : Form
+    {
+        private DateTime finishTime;
+        private Size sizeTopLeftBorder;
+        private bool showUi;
+        private Random rand = new Random();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            sizeTopLeftBorder = GetTopLeftBorderSize();
+            showUi = true;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            finishTime = DateTime.Now.AddMinutes( 25 );
+            //finishTime = DateTime.Now.AddSeconds( 1 );
+            timerUpdateLeftTime.Start();
+            UpdateLeftTimeLabel();
+            HideUi();
+        }
+
+        private void timerUpdateLeftTime_Tick(object sender, EventArgs e)
+        {
+            UpdateLeftTimeLabel();
+        }
+
+        private void UpdateLeftTimeLabel()
+        {
+            TimeSpan diff = 
+                finishTime - DateTime.Now;
+
+            if ( diff.Ticks < 0 )
+            {
+                //---------------------------------------------
+                // Complete!!
+                labelLeftTime.Text = "00:00";
+                timerUpdateLeftTime.Stop();
+                timerShake.Start();
+                return;
+            }
+
+            labelLeftTime.Text = diff.Minutes.ToString("D2") + ":" + diff.Seconds.ToString("D2");
+        }
+
+        private void HideUi()
+        {
+            if (!showUi)
+                return;
+
+            this.SuspendLayout();
+
+            this.FormBorderStyle        = FormBorderStyle.None;
+            this.buttonStart.Visible    = false;
+            this.AllowTransparency      = true;
+            this.TransparencyKey        = SystemColors.Control;
+
+            this.Left   += sizeTopLeftBorder.Width;
+            this.Top    += sizeTopLeftBorder.Height;
+
+            this.TopMost = true;
+
+            this.ResumeLayout(true);
+
+            showUi = false;
+        }
+
+        private void ShowUi()
+        {
+            if (showUi)
+                return;
+
+            this.SuspendLayout();
+
+            this.FormBorderStyle        = FormBorderStyle.FixedToolWindow;
+            this.buttonStart.Visible    = true;
+            this.AllowTransparency      = false;
+
+            this.Left   -= sizeTopLeftBorder.Width;
+            this.Top    -= sizeTopLeftBorder.Height;
+
+            this.TopMost = false;
+
+            this.ResumeLayout(true);
+
+            showUi = true;
+        }
+
+        private void labelLeftTime_MouseEnter(object sender, EventArgs e)
+        {
+            if (timerUpdateLeftTime.Enabled)
+            {
+                ShowUi();
+                timerCheckMouseLeave.Start();
+            }
+
+            if (timerShake.Enabled)
+            {
+                ShowUi();
+                timerShake.Stop();
+                labelLeftTime.ForeColor = SystemColors.ControlText;
+            }
+        }
+
+        // WARN: This emthod is not attached to MainForm.MouseLeave
+        // because that event doesn't support non-client area.
+        private void MainForm_MouseLeave(object sender, EventArgs e)
+        {
+            if ( timerUpdateLeftTime.Enabled )
+                HideUi();
+        }
+
+        private Size GetTopLeftBorderSize()
+        {
+            Rectangle screenRectangle = RectangleToScreen(this.ClientRectangle);
+            return new Size(screenRectangle.Left - this.Left, screenRectangle.Top - this.Top);
+        }
+
+        private void timerCheckMouseLeave_Tick(object sender, EventArgs e)
+        {
+            Point mousePosition = Cursor.Position;
+            if ( mousePosition.X < this.Left || mousePosition.X > this.Right
+                || mousePosition.Y < this.Top || mousePosition.Y > this.Bottom)
+            {
+                timerCheckMouseLeave.Stop();
+                MainForm_MouseLeave(this, null);
+            }
+        }
+
+        private void timerShake_Tick(object sender, EventArgs e)
+        {
+            this.SuspendLayout();
+
+            Left += rand.Next(-20, 20);
+            Top += rand.Next(-20, 20);
+
+            labelLeftTime.ForeColor = Color.FromArgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+
+            this.ResumeLayout(true);
+        }
+
+    }
+}
